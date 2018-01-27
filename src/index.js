63a21fdfa59ac2e731ebfcfd019c6324409e6291
Esdras Barreto <esdras.barreto@gmail.com>
2018-01-27 16:07:51 -0300

    Added new features section 2

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import YTSearch from "youtube-api-search";
 import SearchBar from "./components/search_bar";
 import VideoList from "./components/video_list";
-
+import VideoDetail from "./components/video_detail";
 const API_KEY = "AIzaSyAUf5JBPnBtpjL73OqOWWyNXdyCawh6GOE";
 
 class App extends Component {
@@ -12,7 +12,7 @@ class App extends Component {
 
         this.state = { videos: [] };
 
-        YTSearch({ key: API_KEY, term: "surfboards" }, (videos) => {
+        YTSearch({ key: API_KEY, term: "surfboards" }, videos => {
             this.setState({ videos });
         });
     }
@@ -21,6 +21,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
+                <VideoDetail video={this.state.videos[0]} />
                 <VideoList videos={this.state.videos} />
             </div>
         );
